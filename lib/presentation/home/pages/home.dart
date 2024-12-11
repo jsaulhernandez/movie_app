73857f3c98ecf7bdd161ba1b9d7d923f40473049
Sony <jsaulhernandez98@gmail.com>
2024-12-10 18:16:34 -0600

    tag: finish home page

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -4,6 +4,8 @@ import 'package:movie_app/common/helper/navigation/app_navigation.dart';
 import 'package:movie_app/common/widgets/appbar/app_bar.dart';
 import 'package:movie_app/core/configs/assets/app_vectors.dart';
 import 'package:movie_app/presentation/home/widgets/category_text.dart';
+import 'package:movie_app/presentation/home/widgets/now_playing_movies.dart';
+import 'package:movie_app/presentation/home/widgets/popular_tv.dart';
 import 'package:movie_app/presentation/home/widgets/trending_movies.dart';
 import 'package:movie_app/presentation/search/pages/search.dart';
 
@@ -29,6 +31,25 @@ class HomePage extends StatelessWidget {
           children: [
             CategoryText(title: 'Trendings 🔥'),
             TrendingMovies(),
+            SizedBox(
+              height: 16,
+            ),
+            CategoryText(title: 'Now Playing'),
+            SizedBox(
+              height: 16,
+            ),
+            NowPlayingMovies(),
+            SizedBox(
+              height: 16,
+            ),
+            CategoryText(title: 'Popular TV'),
+            SizedBox(
+              height: 16,
+            ),
+            PopularTv(),
+            SizedBox(
+              height: 16,
+            ),
           ],
         ),
       ),
