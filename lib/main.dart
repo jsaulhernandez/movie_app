5d2752407d931c408360c92f0dac5d565954746d
Sony <jsaulhernandez98@gmail.com>
2024-12-17 19:10:42 -0600

    tag: adding support to RefreshIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/common/bloc/generic_data_cubit.dart';
 import 'package:movie_app/core/configs/theme/app_theme.dart';
 import 'package:movie_app/presentation/splash/bloc/splash_cubit.dart';
 import 'package:movie_app/presentation/splash/pages/splash.dart';
@@ -22,12 +23,16 @@ class MyApp extends StatelessWidget {
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
     );
 
-    return BlocProvider(
-      create: (context) => SplashCubit()..appStarted(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SplashCubit()..appStarted()),
+        BlocProvider(create: (context) => GenericDataCubit()),
+      ],
       child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.appTheme,
-          home: const SplashPage()),
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
+        home: const SplashPage(),
+      ),
     );
   }
 }
--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:movie_app/common/bloc/generic_data_cubit.dart';
 import 'package:movie_app/common/helper/navigation/app_navigation.dart';
 import 'package:movie_app/common/widgets/appbar/app_bar.dart';
 import 'package:movie_app/core/configs/assets/app_vectors.dart';
+import 'package:movie_app/domain/movie/entities/movie.dart';
+import 'package:movie_app/domain/movie/usecases/get_now_playing_movies.dart';
+import 'package:movie_app/domain/movie/usecases/get_trending_movies.dart';
+import 'package:movie_app/domain/tv/entities/tv.dart';
+import 'package:movie_app/domain/tv/usecases/get_popular_tv.dart';
 import 'package:movie_app/presentation/home/widgets/category_text.dart';
 import 'package:movie_app/presentation/home/widgets/now_playing_movies.dart';
 import 'package:movie_app/presentation/home/widgets/popular_tv.dart';
 import 'package:movie_app/presentation/home/widgets/trending_movies.dart';
 import 'package:movie_app/presentation/search/pages/search.dart';
+import 'package:movie_app/service_locator.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,32 +33,45 @@ class HomePage extends StatelessWidget {
         ),
         title: SvgPicture.asset(AppVectors.logo),
       ),
-      body: const SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CategoryText(title: 'Trending 🔥'),
-            TrendingMovies(),
-            SizedBox(
-              height: 16,
-            ),
-            CategoryText(title: 'Now Playing'),
-            SizedBox(
-              height: 16,
-            ),
-            NowPlayingMovies(),
-            SizedBox(
-              height: 16,
-            ),
-            CategoryText(title: 'Popular TV'),
-            SizedBox(
-              height: 16,
-            ),
-            PopularTv(),
-            SizedBox(
-              height: 16,
-            ),
-          ],
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context
+              .read<GenericDataCubit>()
+              .getData<List<MovieEntity>>(sl<GetTrendingMoviesUseCase>());
+          context
+              .read<GenericDataCubit>()
+              .getData<List<MovieEntity>>(sl<GetNowPlayingUseCase>());
+          context
+              .read<GenericDataCubit>()
+              .getData<List<TVEntity>>(sl<GetPopularTvUseCase>());
+        },
+        child: const SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CategoryText(title: 'Trending 🔥'),
+              TrendingMovies(),
+              SizedBox(
+                height: 16,
+              ),
+              CategoryText(title: 'Now Playing'),
+              SizedBox(
+                height: 16,
+              ),
+              NowPlayingMovies(),
+              SizedBox(
+                height: 16,
+              ),
+              CategoryText(title: 'Popular TV'),
+              SizedBox(
+                height: 16,
+              ),
+              PopularTv(),
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
         ),
       ),
     );
